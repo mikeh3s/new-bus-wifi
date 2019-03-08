b12ca5d35e11023c67965992f0e3017c397417db
majomaken <majo.maken@gmail.com>
2019-03-08 12:03:51 -0500

    Comment code

--- homePage/js/contact_form.js
+++ homePage/js/contact_form.js
@@ -1,17 +1,21 @@
 $('#send_btn').click(function send_form(stmt_send){
+    /** Get data values */
     let name = $('#name');
     let email = $('#email');
     let phone = $('#phone');
     let age = $('#age');
     let gender = $('#gender');
+    /**Show alert if success send data or not */
     let warning = $('#warning');
+    /*Get first number phone */
     let num = phone.val().charAt(0);
+    /*Path for file upload data in the server*/
+    let path = "../common/api/proccess.php";
     /** Function validate EMAIL */
     let isEmail = function(email) {
         var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         return regex.test(email.val());
     }
-    
     let data = {
         name: name,
         email: email,
@@ -57,5 +61,18 @@ $('#send_btn').click(function send_form(stmt_send){
     {
         warning.html("Registro exitoso!");
         warning.css('color', 'green');
+
+        $.ajax({
+            type: "POST",
+            url: path,
+            data: data,
+            succes: function(){
+                console.log(data);
+                alert("Nice");
+            },
+            error: function(){
+                alert("Error");
+            }
+        });
     }
 });
